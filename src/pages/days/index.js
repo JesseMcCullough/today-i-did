2ace198ebdc713ad7f5ac60380d3c416c52f050f
Jesse McCullough <jesse@jessem.dev>
2023-07-07 09:28:23 -0400

    Added DayCard to days page

--- src/pages/days/index.js
+++ src/pages/days/index.js
@@ -1,5 +1,24 @@
+import HeaderContainer from "../../components/HeaderContainer/HeaderContainer";
+import DayCard from "../../components/DayCard/DayCard";
+import styles from "./days.module.css";
+
 function Days() {
-    return <h1>I am days</h1>
+    return (
+        <>
+            <HeaderContainer title="Days" buttonText="Add Day" />
+            <div className={styles.dayCardContainer}>
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+                <DayCard date="Friday, July 7" activites={[1, 2, 3]} journal="heeeyyy" />
+            </div>
+        </>
+    )
 }
 
 export default Days;
